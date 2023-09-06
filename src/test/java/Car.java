44e5e79e0a689648be57c35ddf732180ecadbe15
Nikita <nikita.shushevich@gmail.com>
2023-09-06 16:37:52 +0300

    minor changes

--- src/test/java/Car.java
+++ src/test/java/Car.java
@@ -15,15 +15,19 @@ public class Car {
         System.out.println("Car brand: " + brand);
         System.out.println("Car model: " + model);
         System.out.println("Car manufacturing date: " + mnfDate);
+        System.out.println("______________________________");
+
     }
 
+    // main method
     public static void main(String[] args) {
         Car car1 = new Car("Toyota", "Corolla", 2019);
         Car car2 = new Car("Cadillac", "Escalade", 2022);
+        Car car3 = new Car("Mercedes Benz", "S-class", 2010);
 
         car1.displayCarInfo();
-        System.out.println("______________________________");
         car2.displayCarInfo();
+        car3.displayCarInfo();
     }
 }
 
