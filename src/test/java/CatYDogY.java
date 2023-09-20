cf20b46f1437cc6d82095f5cce2c3fb99529a61b
Nikita <nikita.shushevich@gmail.com>
2023-09-20 11:12:29 +0300

    CatYDogY refactored and plain file added

--- src/test/java/CatYDogY.java
+++ src/test/java/CatYDogY.java
@@ -3,20 +3,16 @@ import java.util.Arrays;
 public class CatYDogY {
     public static int[] humanYearsCatYearsDogYears(final int humanYears) {
         int catYears = 15;
-        if(humanYears >= 2){
+        int dogYears = 15;
+
+        if(humanYears >= 2 ){
             catYears += 9;
+            dogYears += 9;
         }
         for (int i = 3; i <= humanYears ; i++) {
             catYears += 4;
-        }
-        int dogYears = 15;
-        if (humanYears >= 2){
-            dogYears += 9;
-        }
-        for (int i = 3; i <= humanYears; i++) {
             dogYears += 5;
         }
-
         return new int[]{humanYears, catYears, dogYears};
     }
 
