6707a9103d6863bf4477062b72e82d6df85d21dd
Nikita <nikita.shushevich@gmail.com>
2023-09-22 16:07:15 +0300

    new task

---
+++ src/test/java/DetectPangram.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DetectPangram {
+    public static boolean check(String sentence){
+        String lower = sentence.toLowerCase();
+        Set<Character> alphabetSet = new HashSet<>(Arrays.asList(
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        ));
+
+        for (char elem : lower.toCharArray()){
+            alphabetSet.remove(elem);
+        }
+        return alphabetSet.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check("abcdefghijklmnopqrstuvwxy"));
+    }
+}
